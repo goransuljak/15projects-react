b6e7952374537a1d162e8f3830ac788356b865f9
Goran <goranfrance@hotmail.com>
2021-02-18 00:30:23 +0000

    afs

--- react-projects-master/3-reviews/setup/src/App.js
+++ react-projects-master/3-reviews/setup/src/App.js
@@ -3,7 +3,16 @@ import Review from './Review';
 
 function App() {
   return <main>
- 
+    <section className="container">
+
+    <div className="title">
+    <h2>Our reviews</h2>
+    <div className="underline">
+    </div>
+    </div>
+    <Review />
+    </section>
+  
   </main>;
 }
 
