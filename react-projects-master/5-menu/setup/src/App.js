a2f235b0f4688d0691631f91a86f1d5349328544
Goran <goranfrance@hotmail.com>
2021-02-23 18:33:39 +0000

    commit

--- react-projects-master/5-menu/setup/src/App.js
+++ react-projects-master/5-menu/setup/src/App.js
@@ -3,9 +3,23 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))] 
+
+
 function App() {
   const [menuItems, setMenuItems] = useState(items)
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if(category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => 
+     item.category === category)
+     setMenuItems(newItems);
+  }
 
   return <main>
     <section className="menu section">
@@ -13,8 +27,8 @@ function App() {
         <h2>Our menu</h2>
         <div className="underline"></div>
       </div>
-      <Categories />
-      <Menu />
+      <Categories categories={categories} filterItems={filterItems} />
+      <Menu  items={menuItems} />
     </section>
   </main>;
 }
